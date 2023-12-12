3c8999c920df9ac8f184c89e1b0c7f6dedd146a3
QiLin Gai <1257382235@qq.com>
2023-12-12 13:10:31 +0800

    add LCR120

---
+++ LCR120.cc
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+class Solution {
+public:
+    int findRepeatDocument(std::vector<int>& documents) {
+        int i = 0;
+        while (i < documents.size()) {
+            if (documents[i] == i) {
+                i ++;
+                continue;
+            }
+            if (documents[documents[i]] == documents[i]) {
+                return documents[i];
+            } else {
+                std::swap(documents[documents[i]], documents[i]);
+            }
+        }
+        return -1;
+    }
+    // int findRepeatDocument(std::vector<int>& documents) {
+    //     std::unordered_map<int, bool> record{};
+    //     for (int d : documents) {
+    //         if (record[d]) {
+    //             return d;
+    //         } else {
+    //             record[d] = true;
+    //         }
+    //     }
+    //     return -1;
+    // }
+};
+
+int main() {
+    Solution s{};
+    std::vector<int> data{2, 5, 3, 0, 5, 0};
+    std::cout << s.findRepeatDocument(data) << std::endl;
+    return 0;
+}
